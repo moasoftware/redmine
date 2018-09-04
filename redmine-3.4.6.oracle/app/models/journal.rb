0d27cc4d33ef3886b5895686541acf58bca49d22
hson@moasoftware.co.kr <hson@moasoftware.co.kr>
2018-09-04 12:50:27 +0900

    solve clob problem in journal notes

--- redmine-3.4.6.oracle/app/models/journal.rb
+++ redmine-3.4.6.oracle/app/models/journal.rb
@@ -40,7 +40,8 @@ class Journal < ActiveRecord::Base
                             :scope => preload({:issue => :project}, :user).
                                       joins("LEFT OUTER JOIN #{JournalDetail.table_name} ON #{JournalDetail.table_name}.journal_id = #{Journal.table_name}.id").
                                       where("#{Journal.table_name}.journalized_type = 'Issue' AND" +
-                                            " (#{JournalDetail.table_name}.prop_key = 'status_id' OR #{Journal.table_name}.notes <> '')").distinct
+                                            " (#{JournalDetail.table_name}.prop_key = 'status_id' OR length(#{Journal.table_name}.notes) > 0)")
+#.distinct
 
   before_create :split_private_notes
   after_commit :send_notification, :on => :create
